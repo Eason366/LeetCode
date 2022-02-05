0f46231f0bd832a1c4473a021ed30c40f37b8a1c
Eason-Wangy366 <754459450@qq.com>
2022-02-05 06:25:17 -0500

    Day2

--- leetcode/src/com/leetcode122.java
+++ leetcode/src/com/leetcode122.java
@@ -9,13 +9,7 @@ public class leetcode122 {
     }
 
     private static int maxProfit(int[] prices) {
-        int profit = 0;
-        for (int i = 0; i<prices.length-1;i++){
-            if (prices[i+1]>prices[i]){
-                profit += prices[i+1] - prices[i];
-            }
-        }
-        return profit;
+        return 0;
     }
 
 }
