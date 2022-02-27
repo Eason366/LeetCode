097b8b34ee5dba55b673868f25f1374593d262de
Eason-Wangy366 <754459450@qq.com>
2022-02-26 22:08:29 -0500

    Day12

--- leetcode/src/com/leetcode203.java
+++ leetcode/src/com/leetcode203.java
@@ -3,22 +3,33 @@ package com;
 public class leetcode203 {
 
     public static void main(String[] args) {
-        int[] nums = new int[] {4,1,2,1,2};
-        int n = singleNumber(nums);
-        System.out.println(n);
-
-    }
-
-    private static int singleNumber(int[] nums) {
-        int ans = 0;
-        for (int num: nums) {
-            ans = ans^num;
+        int[] nums = {7,7,1,1,7,7};
+        ListNode head = new ListNode();
+        for (int n :nums) {
+            head = new ListNode(n,head);
         }
-        return ans;
+        ListNode result = removeElements(head,7);
+        System.out.println(result);
+    }
+
+    private static ListNode removeElements(ListNode head, int val) {
+        ListNode newNode = new ListNode(-1,head);
+        ListNode tempNode = newNode;
+        ListNode headNode = head;
+        while (headNode != null){
+            if (headNode.val == val){
+                tempNode.next = headNode.next;
+            }else {
+                tempNode = headNode;
+            }
+            headNode = headNode.next;
+
+        }
+        return newNode.next;
     }
 
 
-    public class ListNode {
+    public static class ListNode {
       int val;
       ListNode next;
       ListNode() {}
